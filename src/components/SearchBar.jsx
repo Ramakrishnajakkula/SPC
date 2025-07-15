974913c32fba47ea1a2e9796204754c139145785
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-07-15 22:34:41 +0530

    edited search icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -26,28 +26,7 @@ const SearchBar = ({ onSearch, placeholder = "Search...", className = "" }) => {
         isFocused ? "search-bar-focused" : ""
       }`}>
       <div className="search-input-container">
-        <div className="search-icon">
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <circle
-              cx="11"
-              cy="11"
-              r="8"
-              stroke="currentColor"
-              strokeWidth="2"
-            />
-            <path
-              d="M21 21L16.65 16.65"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
-        </div>
+        
         <input
           type="text"
           value={searchTerm}
